53313e0287ff0a2380880d99ecb23663ee2c8e1c
KrzysztofProscinski <krzysztof.proscinski@doctoral.uj.edu.pl>
2023-03-12 11:07:24 +0100

    Update macro.C

--- macro.C
+++ macro.C
@@ -3,19 +3,35 @@ using namespace std;
 
 Int_t macro(){
 
-	cout<<"hello"<<endl;
+	// komentarze (czyli fragmenty kodu, ktore sa ignorowane) mozna pisac na dwa sposoby
 
-/*	Int_t t=0;
-	cout<<t<<endl;*/
+	// sposob 1 - podwojny ukosnik - komentowane jest wszystko na prawo od tych dwoch znakow, ale tylko w jednej linijce
+	// aby odkomentowac fragment kodu nalezy usunac podwojny ukosnik
+
+	/* sposob 2 - ukosnik gwiazdka - komentowane jest wszystko na prawo od tych dwoch znakow az do pary znakow gwiazdka-ukosnik
+	taki komentarz moze zawierac wiecej niz jedna linijke
+	aby odkomentowac fragment kodu nalezy usunac poczatkowe znaki ukosnik-gwiadka i koncowe znaki gwiazdka-ukosnik
+	*/
+
+	// przyklady wypisywania rzeczy w terminalu - "cout"
+	cout<<"hello"<<endl; // napisanie "hello" w terminalu
 	
-/*	Int_t s;
-	cin >> s;
-	cout<<s+1<<endl;*/
+	// inny przyklad
+/*	Int_t t=0; // zdefiniowanie "t" jako liczby calkowitej rownej 0
+	cout<<t<<endl; // wypisanie wartosci "t" w terminalu
+*/
 
-/*	for(Int_t i=0; i<10; i++)
+	// inny przyklad - uzycie petli 
+/*	for(Int_t i=0; i<10; i++) // petla po wartosciach "i" od 0 do 9
 	{
-		cout<<i<<endl;
-	}*/
+		cout<<i<<endl; // wypisanie wartosci "i"
+	}
+*/	
 
+	// przyklady wpisywanie rzeczy z klawiatury - "cin"
+/*	Int_t s; // zdefiniowanie "s" jako liczby calkowitej
+	cin >> s; // wartosc "s" nalezy wpisac w terminalu uzywajac klawiatury
+	cout<<s+1<<endl; // wypisanie wartosci "s+1" w terminalu (dla testu czy ta metoda dziala)
+*/
 	return 0;
 }
