7b9d21c269e9a1c7d268bf607597b200eba7b0cb
KrzysztofProscinski <krzysztof.proscinski@doctoral.uj.edu.pl>
2023-03-12 11:17:24 +0100

    Update wykres.C

--- wykres.C
+++ wykres.C
@@ -1,11 +1,16 @@
 {
-/*	Double_t x[] = {1.,2.,3.,4.,5.};
-	Double_t y[]={0.,1.,0.,2.,3.};
-	TGraph plot(5,x,y);
+	
+	// przyklad 1 - wypelnianie wykresu elementami tablic	
+/*	Double_t a[] = {1.,2.,3.,4.,5.}; // definicja tablicy a
+	Double_t b[]={0.,1.,0.,2.,3.}; // definicja tablicy b
+	TGraph plot(5,a,b); // wypelnianie wykresu
+	// 5 - liczba punktow na wykresie (powinna byc nie wieksza niz liczba elementow tablic), a - tablica z wartosciami z osi X, b - tablica z wartosciami z osi Y
 */
 
-	TGraph plot("dane.dat");
+	// przyklad 2 - wypelnianie wykresu danymi z pliku "dane.dat"
+//	TGraph plot("dane.dat");
 
 
+	// rysowanie wykresu
 	plot.Draw();
 }
