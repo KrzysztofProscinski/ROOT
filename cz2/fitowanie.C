b27acd08e790cadd12890d8bd61e6612fc972828
KrzysztofProscinski <krzysztof.proscinski@doctoral.uj.edu.pl>
2023-03-22 14:18:39 +0100

    Update fitowanie.C

--- cz2/fitowanie.C
+++ cz2/fitowanie.C
@@ -6,12 +6,15 @@
 
 using namespace std;
 
+// definicja funkcji
 Double_t MyFunction(Double_t *arg, Double_t *par){
-	Double_t f = par[0]+par[1]*arg[0]+par[2]*arg[0]*arg[0];
+	Double_t f = par[0]+par[1]*arg[0]+par[2]*arg[0]*arg[0]; // funkcja bedzia miala postac a_0 + a_1 * x + a_2 * x^2, gdzie x - zmienna, a - parametry
 	return f;}
 
+// glowna czesc makra
 Bool_t fitowanie(void){
 
+// histogram, do ktorego bedzie fitowana funkcja
    TH1F *hist = new TH1F("h","Histogram",100,0.,20.);
    for(Int_t i=0; i<100; i++){
 	hist->SetBinContent(i,i);
@@ -19,10 +22,11 @@ Bool_t fitowanie(void){
 
    hist->Draw();
 
-   TF1 *f1 = new TF1("funkcja",MyFunction,0,100,3);
-   f1->SetParameters(0.,-2.,1.);
-   TFitResultPtr results = hist->Fit(f1,"S");
-   cout << "Chi2 = " << results->Chi2() << endl;
+// funkcja, ktora bedzie fitowana
+   TF1 *f1 = new TF1("funkcja",MyFunction,0,100,3); // postac funkcji
+   f1->SetParameters(0.,-2.,1.); // parametry poczatkowe
+   TFitResultPtr results = hist->Fit(f1,"S"); // fitowanie
+   cout << "Chi2 = " << results->Chi2() << endl; // wypisanie wartosci chi^2 z fitu
 
    return kTRUE;
 }
