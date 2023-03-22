b587e4673ca1faaec1814af023711e005271ba5d
KrzysztofProscinski <krzysztof.proscinski@doctoral.uj.edu.pl>
2023-03-22 14:13:11 +0100

    Update Ntuple.C

--- cz2/Ntuple.C
+++ cz2/Ntuple.C
@@ -1,16 +1,16 @@
 {
-   Float_t energy, momentum, charge;
-   TFile f ("ntuple.root","RECREATE");
+   Float_t energy, momentum, charge; // definicja parametrow, ktore beda uzywane w petli
+   TFile f ("ntuple.root","RECREATE"); // definicja pliku do ktorego beda zapisane rzeczy
    
-   TNtuple Nt ("ntuple","N tuple 1","Energy:Momentum:Charge");
+   TNtuple Nt ("ntuple","N tuple 1","Energy:Momentum:Charge"); // definicja Ntupli, w ktorej beda wartosci energi, pedu i ladunku
    TRandom3 r;
    r.SetSeed();
-   for(Int_t i=0; i<1000; i++){
-	energy = r.Gaus();
-	momentum = r.Gaus();
-	charge=1;
-	Nt.Fill(energy,momentum,charge);
+   for(Int_t i=0; i<1000; i++){ // petla do wypelniania Ntupli
+	energy = r.Gaus(); // energia bedzie generowany losowo wg rozkladu Gaussa
+	momentum = r.Gaus(); // ped bedzie generowany losowo wg rozkladu Gaussa
+	charge=1; // ladunek bedzie zawsze rowny 1
+	Nt.Fill(energy,momentum,charge); // wypelnianie Ntupli
    }
    
-   Nt.Write();
+   Nt.Write(); // zapis Ntupli
 }
