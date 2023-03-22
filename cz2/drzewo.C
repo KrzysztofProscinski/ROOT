65d293e10d4c958c61af5cd948f828012343722e
KrzysztofProscinski <krzysztof.proscinski@doctoral.uj.edu.pl>
2023-03-22 14:15:27 +0100

    Update drzewo.C

--- cz2/drzewo.C
+++ cz2/drzewo.C
@@ -1,19 +1,19 @@
 {
-   Float_t energy, momentum;
+   Float_t energy, momentum; // definicja parametrow do uzycia w petli
    Int_t charge;
-   TFile f ("tree.root","RECREATE");
-   TTree* t = new TTree("tree","Tree 1");
-   t->Branch("E",&energy,"Energy/F");
+   TFile f ("tree.root","RECREATE"); // definicja pliku w ktorym drzewo bedzie zapisywane
+   TTree* t = new TTree("tree","Tree 1"); // definicja drzewa
+   t->Branch("E",&energy,"Energy/F"); // ustawienie galezi drzewa
    t->Branch("mom",&momentum,"Momentum/F");
    t->Branch("charge",&charge,"Charge/B");
 
    TRandom3 r;
    r.SetSeed();
-   for(Int_t i=0; i<1000; i++){
-	energy = r.Gaus();
-	momentum = r.Gaus();
-	charge = 1;
-	t->Fill();
+   for(Int_t i=0; i<1000; i++){ // petla do wypelniania drzewa
+	energy = r.Gaus(); // energia bedzie generowany losowo wg rozkladu Gaussa
+	momentum = r.Gaus(); // ped bedzie generowany losowo wg rozkladu Gaussa
+	charge = 1; // ladunek bedzie rowny 1
+	t->Fill(); // wypelnianie drzewa
    }
-   t->Write();
+   t->Write(); // zapisywanie drzewa
 }
